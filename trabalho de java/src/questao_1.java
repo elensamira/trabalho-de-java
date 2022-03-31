73b802dec5da1358f22d5ce6504e4c41173a4a9b
elensamira <elen.feitosa@aluno.ce.gov.br>
2022-03-31 14:14:36 -0300

    Trabalho

---
+++ trabalho de java/src/questao_1.java
@@ -0,0 +1,14 @@
+public class questao_1 {
+
+	public static void main(String[]args) {
+		double PI = 3.14159 ;
+		int raio = 12 ;
+		double area = PI * (raio * raio);
+		System.out.println("o valor do raio e: "+ area);
+		
+		
+	}
+	
+	
+	
+}
---
+++ trabalho de java/src/questao_2.java
@@ -0,0 +1,31 @@
+public class questao_2 {
+
+	public static void main(String[]args) {
+		
+		double a = 2.9;
+		double b = 3.6;
+		double c = 5.8;
+		double pi = 3.14159;
+		 
+		double areatriangulo = a * c;
+		System.out.println("o resultado e: "+areatriangulo);
+		
+		double areacirculo = pi * (c * c);
+		System.out.println("o resultado e: "+areacirculo);
+		
+		double areatrapezio = (a+b) * c / 2;
+		System.out.println("o resultado e: "+areatrapezio);
+		
+		double areaquadrado = b * b;
+		System.out.println("o resultado e: "+areaquadrado);
+		
+		double arearetangulo = a * b;
+		System.out.println("o resultado e: "+arearetangulo);
+		
+		
+		
+		
+	}
+	
+	
+}
